376ec2c4921373285daf39541b12004919995d93
Huisheng Liu <huishengliu@hotmail.com>
2017-09-15 23:20:24 -0700

    initial done

--- 2.1.1.RemoveDuplicates/211RemoveDuplicates.cpp
+++ 2.1.1.RemoveDuplicates/211RemoveDuplicates.cpp
@@ -3,6 +3,24 @@
 
 #include "stdafx.h"
 
+int RemoveDuplicates(int sorted_numbers[], int size)
+{
+    if (size <= 1) return size;
+
+    int i = 0;
+    int j = 1;
+
+    while (j < size)
+    {
+        while (sorted_numbers[j] == sorted_numbers[i]) ++j;
+        if (j<size)
+        {
+            sorted_numbers[++i] = sorted_numbers[j++];
+        }
+    }
+
+    return i+1;
+}
 
 int main()
 {
