04d55c4bab9c79a3e533d85265f3eda83003acd2
Huisheng Liu <huishengliu@hotmail.com>
2017-09-18 23:22:41 -0700

    Updated.

--- 2.1.1.RemoveDuplicates/211RemoveDuplicates.cpp
+++ 2.1.1.RemoveDuplicates/211RemoveDuplicates.cpp
@@ -1,4 +1,5 @@
 // 211RemoveDuplicates.cpp : Defines the entry point for the console application.
+// Solution 3*: no nested loop, clean code.
 //
 
 #include "stdafx.h"
@@ -27,6 +28,8 @@ int RemoveDuplicates2(int sorted_numbers[], int size)
     int i = 0;
     int j = 0;
 
+	if (size == 0) return 0;
+
     while (j < size)
     {
         sorted_numbers[i++] = sorted_numbers[j++];
@@ -41,6 +44,8 @@ int RemoveDuplicates3(int sorted_numbers[], int size)
     int i = 0;
     int j = 0;
 
+	if (size == 0) return 0;
+
     while (j < size)
     {
         if (i == j || sorted_numbers[i] == sorted_numbers[j])
@@ -50,6 +55,59 @@ int RemoveDuplicates3(int sorted_numbers[], int size)
         }
         sorted_numbers[++i] = sorted_numbers[j++];
     }
+
+	return i + 1;
+}
+
+int RemoveDuplicates3_1(int sorted_numbers[], int size)
+{
+	int i = 0;
+	int j = 0;
+
+	if (size == 0) return 0;
+
+	while (j < size)
+	{
+		if (i != j && sorted_numbers[i] != sorted_numbers[j])
+		{
+			sorted_numbers[++i] = sorted_numbers[j];
+		}
+		j++;
+	}
+
+	return i + 1;
+}
+
+int RemoveDuplicates3_2(int sorted_numbers[], int size)
+{
+	int i = 0;
+
+	if (size == 0) return 0;
+
+	for (int j = 0; j < size; j++)
+	{
+		if (i != j && sorted_numbers[i] != sorted_numbers[j])
+		{
+			sorted_numbers[++i] = sorted_numbers[j];
+		}
+	}
+
+	return i + 1;
+}
+
+int RemoveDuplicates3_3(int sorted_numbers[], int size)
+{
+	int i = 0;
+
+	if (size == 0) return 0;
+
+	for (int j = 1; j < size; j++)
+	{
+		if (sorted_numbers[i] != sorted_numbers[j])
+		{
+			sorted_numbers[++i] = sorted_numbers[j];
+		}
+	}
 
 	return i + 1;
 }
@@ -75,9 +133,17 @@ int main()
     print(sn2, s2);
 
 	int sn3[] = { 1, 1, 2, 2, 2, 2, 3, 4, 5, 5, 5, 5 };
-    int s3 = RemoveDuplicates(sn3, sizeof(sn3)/sizeof(int));
+    int s3 = RemoveDuplicates3(sn3, sizeof(sn3)/sizeof(int));
     print(sn3, s3);
 
-    return 0;
+	int sn3_1[] = { 1, 1, 2, 2, 2, 2, 3, 4, 5, 5, 5, 5 };
+	int s3_1 = RemoveDuplicates3_1(sn3_1, sizeof(sn3_1) / sizeof(int));
+	print(sn3_1, s3_1);
+
+	int sn3_2[] = { 1, 1, 2, 2, 2, 2, 3, 4, 5, 5, 5, 5 };
+	int s3_2 = RemoveDuplicates3_2(sn3_2, sizeof(sn3_2) / sizeof(int));
+	print(sn3_2, s3_2);
+
+	return 0;
 }
 
